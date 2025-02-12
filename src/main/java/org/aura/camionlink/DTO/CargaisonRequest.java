9418755e0da3f4bddad98eb90fbc57ac950b0d9e
salah <akilsalaheddine203@gmail.com>
2025-02-12 23:27:05 +0100

    add cargaison mapper

--- src/main/java/org/aura/camionlink/DTO/CargaisonRequest.java
+++ src/main/java/org/aura/camionlink/DTO/CargaisonRequest.java
@@ -5,8 +5,9 @@ import org.aura.camionlink.Entities.Enums.CargaisonType;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.Builder;
 
-
+@Builder
 public record CargaisonRequest(
     @NotBlank(message = "La description ne doit pas être vide")
     String description,
--- src/main/java/org/aura/camionlink/Mapper/CargaisonMapper.java
+++ src/main/java/org/aura/camionlink/Mapper/CargaisonMapper.java
@@ -1,4 +1,21 @@
 package org.aura.camionlink.Mapper;
 
+import org.aura.camionlink.DTO.CargaisonRequest;
+import org.aura.camionlink.DTO.CargaisonResponse;
+import org.aura.camionlink.Entities.Cargaison;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel =  "spring")
 public interface CargaisonMapper {
+    @Mapping(target = "trajet" , ignore = true)
+    @Mapping(target = "id" , ignore = true)
+    Cargaison toEntity (CargaisonRequest cargaisonRequest);
+    CargaisonResponse toCargaisonResponse (Cargaison cargaison);
+
+    @Mapping(target = "trajet" , ignore = true)
+    @Mapping(target = "id" , ignore = true)
+    void updateCargaisonFromRequset(CargaisonRequest cargaisonRequest 
+    , @MappingTarget Cargaison cargaison );
 }
