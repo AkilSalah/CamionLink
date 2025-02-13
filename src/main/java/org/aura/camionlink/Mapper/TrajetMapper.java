cc153076d7406d887d245e61b05b09e8a1e05804
salah <akilsalaheddine203@gmail.com>
2025-02-13 11:56:47 +0100

    add mapper and service interface trajet

--- src/main/java/org/aura/camionlink/Mapper/TrajetMapper.java
+++ src/main/java/org/aura/camionlink/Mapper/TrajetMapper.java
@@ -1,5 +1,27 @@
 package org.aura.camionlink.Mapper;
 
+import org.aura.camionlink.DTO.TrajetRequest;
+import org.aura.camionlink.DTO.TrajetResponse;
+import org.aura.camionlink.Entities.Trajet;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface TrajetMapper {
     
-}
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "conducteur", ignore = true)
+    @Mapping(target = "camion", ignore = true)
+    @Mapping(target = "cargaison", ignore = true)
+    @Mapping(target = "depenses", ignore = true)
+    Trajet toEntity(TrajetRequest request);
+
+    TrajetResponse toTrajetResponse(Trajet trajet);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "conducteur", ignore = true)
+    @Mapping(target = "camion", ignore = true)
+    @Mapping(target = "cargaison", ignore = true)
+    @Mapping(target = "depenses", ignore = true)
+    void updateTrajetFromRequest(TrajetRequest request, @MappingTarget Trajet trajet);}
--- src/main/java/org/aura/camionlink/Services/Interface/TrajetService.java
+++ src/main/java/org/aura/camionlink/Services/Interface/TrajetService.java
@@ -1,4 +1,14 @@
 package org.aura.camionlink.Services.Interface;
 
+import java.util.List;
+
+import org.aura.camionlink.DTO.TrajetRequest;
+import org.aura.camionlink.DTO.TrajetResponse;
+
 public interface TrajetService {
+    TrajetResponse createTrajet(TrajetRequest request);
+    TrajetResponse getTrajetById(Long id);
+    List<TrajetResponse> getAllTrajets();
+    TrajetResponse updateTrajet(Long id, TrajetRequest request);
+    void deleteTrajet(Long id);
 }
