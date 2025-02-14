272c79c69966d9227e79675027d3273a7f801b95
salah <akilsalaheddine203@gmail.com>
2025-02-14 20:36:18 +0100

    add depense mapper

--- src/main/java/org/aura/camionlink/Mapper/DepenseMapper.java
+++ src/main/java/org/aura/camionlink/Mapper/DepenseMapper.java
@@ -1,4 +1,22 @@
 package org.aura.camionlink.Mapper;
 
+import org.aura.camionlink.DTO.DepenseRequest;
+import org.aura.camionlink.DTO.DepenseResponse;
+import org.aura.camionlink.Entities.Depense;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface DepenseMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "trajet",ignore = true)
+    Depense toEntity(DepenseRequest request);
+    
+    DepenseResponse toResponse(Depense depense);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "trajet",ignore = true)
+    void updateDepenseFromRequest(DepenseRequest request , @MappingTarget Depense ddepense);
+
 }
