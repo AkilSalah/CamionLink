8395d7904435c9e9b78de1ac8b1e3fc8c07b97f6
salah <akilsalaheddine203@gmail.com>
2025-02-14 20:24:50 +0100

    add methode in repository for get all trajet depenses

--- src/main/java/org/aura/camionlink/Repositories/DepenseRepo.java
+++ src/main/java/org/aura/camionlink/Repositories/DepenseRepo.java
@@ -1,7 +1,11 @@
 package org.aura.camionlink.Repositories;
 
+import java.util.List;
+
 import org.aura.camionlink.Entities.Depense;
+import org.aura.camionlink.Entities.Trajet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepenseRepo extends JpaRepository<Depense,Long> {
+    List<Depense> findByTrajet(Trajet trajet);
 }
