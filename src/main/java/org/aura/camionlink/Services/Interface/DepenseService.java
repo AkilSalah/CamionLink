52a9012e9c5c0b61f4548995dd145b413e1ec1da
salah <akilsalaheddine203@gmail.com>
2025-02-14 20:22:07 +0100

    add depense interface

--- src/main/java/org/aura/camionlink/Services/Interface/DepenseService.java
+++ src/main/java/org/aura/camionlink/Services/Interface/DepenseService.java
@@ -1,4 +1,14 @@
 package org.aura.camionlink.Services.Interface;
 
+import java.util.List;
+
+import org.aura.camionlink.DTO.DepenseRequest;
+import org.aura.camionlink.DTO.DepenseResponse;
+
 public interface DepenseService {
+    List<DepenseResponse> getDepenses();
+    List<DepenseResponse> getDepenseByTrajet(long trajetId);
+    DepenseResponse createDepense(DepenseRequest request);
+    DepenseResponse updateDepense(DepenseRequest request);
+    void deleteDepense(long id);   
 }
