249956049adcc0fbf998b863c436dbf7973b22ff
salah <akilsalaheddine203@gmail.com>
2025-02-23 01:22:03 +0100

    add entretien repository

--- src/main/java/org/aura/camionlink/Repositories/EntretienRepo.java
+++ src/main/java/org/aura/camionlink/Repositories/EntretienRepo.java
@@ -1,5 +1,8 @@
 package org.aura.camionlink.Repositories;
 
 
-public interface EntretienRepo {
+import org.aura.camionlink.Entities.Entretien;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntretienRepo extends JpaRepository<Entretien,Long> {
 }
